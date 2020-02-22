ad77a6fb2fe4537b19146b9a5d094a8e535d4f14
Goncalo Alves <al.darkseidd.2010@gmail.com>
2020-02-22 18:44:30 +0000

    Reading Keystrokes

--- TP_020/src/main/java/Game.java
+++ TP_020/src/main/java/Game.java
@@ -1,13 +1,18 @@
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x =10;
+    private int y=10;
     Game(){
         try {
             Terminal terminal = new DefaultTerminalFactory().createTerminal();
@@ -23,7 +28,7 @@ public class Game {
     }
     private void draw(){
         screen.clear();
-        screen.setCharacter(10, 10, new TextCharacter('X'));
+        screen.setCharacter(x, y, new TextCharacter('X'));
         try {
             screen.refresh();
         } catch (IOException e) {
@@ -31,6 +36,44 @@ public class Game {
         }
     }
     public void run(){
-        draw();
+        while (true){
+            draw();
+            try {
+                com.googlecode.lanterna.input.KeyStroke key = screen.readInput();
+                processKey(key);
+                if(KeyType.EOF == key.getKeyType())
+                    break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+    }
+    private void processKey(KeyStroke key){
+        if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q'){
+            try {
+                screen.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        switch (key.getKeyType()){
+            case ArrowDown:
+                y+=1;
+                break;
+            case ArrowLeft:
+                x-=1;
+                break;
+            case ArrowRight:
+                x+=1;
+                break;
+            case ArrowUp:
+                y-=1;
+                break;
+        }
+
     }
 }
