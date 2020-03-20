824812fb1684bbc3d20aefcc5f048c4a64463be2
Goncalo Alves <al.darkseidd.2010@gmail.com>
2020-03-20 12:31:25 +0000

    ex3 done

---
+++ TP_04/area-calculator/src/main/java/Line.java
@@ -0,0 +1,14 @@
+public class Line implements BasicShape {
+    private int length;
+    public Line(int length){
+        this.length=length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
