f29586c0ef4d1f1add6bbe28c69711a8e0191b1d
Goncalo Alves <al.darkseidd.2010@gmail.com>
2020-03-27 18:02:40 +0000

    Added ex~4

--- design_patterns/src/main/java/HumanClient.java
+++ design_patterns/src/main/java/HumanClient.java
@@ -1,16 +1,18 @@
 public class HumanClient implements Client {
+    private OrderingStrategy strategy;
+    public HumanClient(OrderingStrategy strategy){this.strategy=strategy;}
     @Override
     public void wants(StringDrink drink, StringRecipe recipe, StringBar bar) {
-
+        strategy.wants(drink,recipe,bar);
     }
 
     @Override
     public void happyHourStarted(Bar bar) {
-        bar.isHappyHour();
+        strategy.happyHourStarted((StringBar) bar);
     }
 
     @Override
     public void happyHourEnded(Bar bar) {
-        bar.endHappyHour();
+        strategy.happyHourStarted((StringBar) bar);
     }
 }
